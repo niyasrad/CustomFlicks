fcd3ec3ee44e95df687422b6acb19a61c8de8e0e
Niyas Hameed <niyasrad@gmail.com>
2023-08-25 15:13:41 +0530

    feat: built day-recommendation component (#7)

---
+++ frontend/src/components/dayrec/DayRec.styles.ts
@@ -0,0 +1,131 @@
+import { styled } from "styled-components";
+
+export const DayRecWrapper = styled.div`
+    width: 90%;
+    padding: 3rem;
+    box-sizing: border-box;
+    cursor: pointer;
+    margin: auto;
+    margin-top: 2rem;
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+    border-radius: 2rem;
+    background-color: black;
+    box-shadow:  0px 4px 20px 0px ${ props => props.theme.primary };
+
+    img {
+        width: 100%;
+        max-width: 50%;
+        height: auto;
+        object-fit: cover;
+        border-radius: 2rem;
+    }
+
+    @media only screen and (max-width: 990px) {
+        width: 85%;
+        flex-direction: column;
+        padding: 2rem;
+
+        img {
+            max-width: 100%;
+        }
+    }
+`
+
+export const DayRecDetails = styled.div`
+    max-width: 60%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 1.5rem;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5rem;
+    }
+
+    .dayrec__link {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    .dayrec__link h6 {
+        overflow: hidden;
+        -webkit-box-orient: vertical;
+    }
+
+    .dayrec__link button {
+        padding: 0.5rem 2rem;
+        text-align: center;
+        border: none;
+        color: white;
+        border-radius: 2rem;
+        font-size: 1.1em;
+        background-color: ${ props => props.theme.accentsec};
+        cursor: pointer;
+    }
+
+    .dayrec__genres {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+
+    h4 {
+        letter-spacing: 0.5em;
+        padding-top: 1rem;
+        color: white;
+        font-size: 1.5em;
+        overflow: hidden;
+    }
+
+    h6 {
+        color: ${ props => props.theme.primary };
+        font-size: 1.3em;
+    }
+
+    p {
+        color: white;
+        letter-spacing: 0.1em;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+    }
+
+    @media only screen and (max-width: 990px) {
+        max-width: 100%;
+
+        h4 {
+            font-size: 1.3em;
+        }
+
+        h6 {
+            font-size: 1.1em;
+            letter-spacing: 0.1em;
+        }
+
+        p {
+            font-size: 0.9em;
+        }
+    }
+`
+
+export const GenreName = styled.div`
+    width: auto;
+    padding: 0.5rem 1.5rem;
+    text-align: center;
+    border-radius: 2rem;
+    background-color: ${ props => props.theme.accentsec}80;
+
+    p {
+        color: white;
+        font-size: 0.9em;
+        letter-spacing: 0.1em;
+    }
+`
---
+++ frontend/src/components/dayrec/DayRec.tsx
@@ -0,0 +1,59 @@
+import { DayRecDetails, DayRecWrapper, GenreName } from "./DayRec.styles";
+
+export function RecGenre({ genre }: { genre: string }) {
+    return (
+        <GenreName>
+            <p>{genre}</p>
+        </GenreName>
+    )
+}
+
+interface RecResponse {
+    title: string;
+    desc: string;
+    genres: Array<string>;
+    poster: string;
+    redir: string;
+}
+
+const sampleResponse: RecResponse = {
+    title: "Sinister 2",
+    desc: "Courtney Collins, a young mother, and her twin sons move into a haunted rural house and start experiencing a string of supernatural occurrences linked with the demon Bughuul.",
+    genres: ['Horror', 'Fun', 'Raunchy'],
+    poster: "https://images7.alphacoders.com/614/614609.jpg",
+    redir: "https://www.youtube.com/watch?v=fChx_YZUAR0"
+}
+
+export default function DayRec() {
+
+    return (
+        <DayRecWrapper>
+            <img
+                src={sampleResponse.poster}
+                alt="Movie Poster"
+            />
+            <DayRecDetails>
+                <div className="dayrec__link">
+                    <h6>CF Recommends</h6>
+                    <a 
+                        href={sampleResponse.redir}
+                        target="_blank"
+                    >
+                        <button>WATCH</button>
+                    </a>
+                </div>
+                <div>
+                    <h4>{sampleResponse.title}</h4>
+                    <p>{sampleResponse.desc}</p>
+                </div>
+                <div>
+                    <h6>Genres</h6>
+                    <div className="dayrec__genres">
+                        { sampleResponse.genres.map((genre: string) => <RecGenre genre={genre} />) }
+                    </div>
+                </div>
+
+            </DayRecDetails>
+        </DayRecWrapper>
+    )
+}
